7f5eb2f524b8180e8d585766d573317abb1c0408
Guillaume Lazaro <guillaume.lazaro@webinage.fr>
2020-04-17 10:13:07 +0200

    Add Tweet model

--- database/models/tweet.model.js
+++ database/models/tweet.model.js
@@ -2,5 +2,14 @@ const mongoose = require('mongoose');
 const schema = mongoose.schema();
 
 const tweetSchema = schema({
-
+    content: {
+        type: String,
+        maxLength: 140,
+        minLength: 1,
+        require: true
+    }
 })
+
+const Tweet = mongoose.model('tweet');      //Transforme le schema de données en Modéle
+
+module.exports = Tweet;
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/', (req,res) => {
     res.render("home");
 });
 
+router.get('/tweet/new', (req,res) => {
+    res.render("tweets/tweet-form");
+});
+
 module.exports = router;
